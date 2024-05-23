ff37569924aa22cf42cbb896fc4085af70cd438c
Anna Bargamotova <anna.samoilenko.2012@gmail.com>
2024-05-23 18:03:39 +0900

    added transitions to theme switch

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,8 +1,8 @@
-import styles from "./styles.module.css";
-import { formatTimeAgo } from "../../helpers/formatTimeAgo";
-import Image from "../image/Image";
-import withSkeleton from "../../helpers/hocs/withSkeleton";
-import { INews } from "../../interfaces";
+import styles from './styles.module.css';
+import { formatTimeAgo } from '../../helpers/formatTimeAgo';
+import Image from '../image/Image';
+import withSkeleton from '../../helpers/hocs/withSkeleton';
+import { INews } from '../../interfaces';
 
 interface Props {
   banner: INews;
@@ -16,16 +16,15 @@ const Banner = ({ banner }: Props) => {
     <article className={styles.banner}>
       <div className={styles.banner__info}>
         <h3 className={styles.banner__title}>{banner?.title}</h3>
-
         <p className={styles.banner__extra}>
           {formatTimeAgo(banner?.published)} by {banner?.author}
         </p>
       </div>
-      {imageExist ? <Image image={banner?.image} /> : ""}
+      {imageExist ? <Image image={banner?.image} /> : ''}
     </article>
   );
 };
 
-const BannerWithSkeleton = withSkeleton(Banner, "banner", 1);
+const BannerWithSkeleton = withSkeleton(Banner, 'banner', 1);
 
 export default BannerWithSkeleton;
