9f2e330c2c60de4f36ae7b3c4ebf237b9d848c2a
Anna Bargamotova <anna.samoilenko.2012@gmail.com>
2024-05-23 17:46:31 +0900

    fixing some problem with pagination

--- src/components/newsByFilter/NewsByFilter.tsx
+++ src/components/newsByFilter/NewsByFilter.tsx
@@ -1,20 +1,21 @@
-import styles from "./styles.module.css";
-import NewsList from "../newsList/NewsList";
-import { TOTAL_PAGES } from "../../constant/constants";
-import { useDebounce } from "../../helpers/hooks/useDebounce";
-import Filters from "../newsFilters/NewsFilters";
-import Title from "../title/Title";
-import Banner from "../banner/Banner";
-import PaginationWrapper from "../paginationWrapper/PaginationWrapper";
-import Line from "../line/Line";
-import { useGetNewsQuery } from "../../store/services/newsApi";
-import { useAppDispatch, useAppSelector } from "../../store";
-import { setFilters } from "../../store/slices/newsSlice";
+import styles from './styles.module.css';
+import NewsList from '../newsList/NewsList';
+import { TOTAL_PAGES } from '../../constant/constants';
+import { useDebounce } from '../../helpers/hooks/useDebounce';
+import Filters from '../newsFilters/NewsFilters';
+import Title from '../title/Title';
+import Banner from '../banner/Banner';
+import PaginationWrapper from '../paginationWrapper/PaginationWrapper';
+import Line from '../line/Line';
+import { useGetNewsQuery } from '../../store/services/newsApi';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { setFilters } from '../../store/slices/newsSlice';
 
 const NewsByFilter = () => {
   const dispatch = useAppDispatch();
   const filters = useAppSelector((state) => state.news.filters);
   const news = useAppSelector((state) => state.news.news);
+  console.log(news);
 
   const debouncedKeywords = useDebounce(filters.keywords, 1500);
   const { isLoading } = useGetNewsQuery({
@@ -26,19 +27,19 @@ const NewsByFilter = () => {
   const handleNextPage = () => {
     if (filters.page_number < TOTAL_PAGES) {
       dispatch(
-        setFilters({ key: "page_number", value: filters.page_number + 1 })
+        setFilters({ key: 'page_number', value: filters.page_number + 1 }),
       );
     }
   };
   const handlePrevPage = () => {
     if (filters.page_number > 1) {
       dispatch(
-        setFilters({ key: "page_number", value: filters.page_number - 1 })
+        setFilters({ key: 'page_number', value: filters.page_number - 1 }),
       );
     }
   };
   const handlePageClick = (pageNumber: number) => {
-    dispatch(setFilters({ key: "page_number", value: pageNumber }));
+    dispatch(setFilters({ key: 'page_number', value: pageNumber }));
   };
 
   return (
@@ -46,11 +47,11 @@ const NewsByFilter = () => {
       <div className={styles.wrapper}>
         <div className={styles.inner}>
           <Filters filters={filters} />
-          <Title title={filters.category ? filters.category : "Word news"} />
+          <Title title={filters.category ? filters.category : 'Word news'} />
           <Banner isLoading={isLoading} banner={news && news[0]} />
           <Line />
           <PaginationWrapper
-            top={true}
+            top={false}
             bottom={true}
             totalPages={TOTAL_PAGES}
             handleNextPage={handleNextPage}
